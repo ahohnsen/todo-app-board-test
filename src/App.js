0027782a2911ab7dc67f5d5ac33007bc027bd83d
Jeremias Erbs <jerry@badabam.de>
2022-06-20 11:42:06 +0200

    WIP: add useSWR

--- src/App.js
+++ src/App.js
@@ -1,80 +1,91 @@
-import { nanoid } from 'nanoid'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import useSWR from 'swr'
 import TodoForm from './components/TodoForm.js'
 import TodoItem from './components/TodoItem.js'
 
-function App() {
-  const [todos, setTodos] = useState([])
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
-  useEffect(() => {
-    fetch('/api/todos')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Not okay on GET todos')
-        }
-        return response.json()
-      })
-      .then(todos => setTodos(todos))
-      .catch(error => console.log(error))
-  }, [])
+function App() {
+  // const [todos, setTodos] = useState([])
+
+  const { data: todos, error, isValidating } = useSWR('/api/todos', fetcher)
+
+  // useEffect(() => {
+  //   fetch('/api/todos')
+  //     .then(response => {
+  //       if (!response.ok) {
+  //         throw new Error('Not okay on GET todos')
+  //       }
+  //       return response.json()
+  //     })
+  //     .then(todos => setTodos(todos))
+  //     .catch(error => console.log(error))
+  // }, [])
 
   return (
-    <Grid>
-      <TodoList>
-        {todos.map((todo, index) => (
-          <TodoItem
-            key={todo._id}
-            text={todo.description}
-            id={todo._id}
-            isDone={todo.done}
-            onToggle={() => toggleTodo(index)}
-          />
-        ))}
-      </TodoList>
-      <TodoForm onCreateTodo={addTodo} />
-    </Grid>
+    <>
+      <Grid>
+        {isValidating && '... Loading ...'}
+        {!error && !todos && 'No data'}
+        {!error && todos ? (
+          <TodoList>
+            {todos?.map((todo, index) => (
+              <TodoItem
+                key={todo._id}
+                text={todo.description}
+                id={todo._id}
+                isDone={todo.done}
+                onToggle={() => toggleTodo(index)}
+              />
+            ))}
+          </TodoList>
+        ) : (
+          'No todos'
+        )}
+        <TodoForm onCreateTodo={addTodo} />
+      </Grid>
+    </>
   )
 
   function addTodo(text) {
-    fetch('/api/todos', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ description: text }),
-    })
-      .then(response => {
-        if (!response.ok) throw new Error('POST todo not ok')
-        return response.json()
-      })
-      .then(todo => setTodos([...todos, todo]))
-      .catch(error => console.log(error))
+    // fetch('/api/todos', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({ description: text }),
+    // })
+    //   .then(response => {
+    //     if (!response.ok) throw new Error('POST todo not ok')
+    //     return response.json()
+    //   })
+    //   .then(todo => setTodos([...todos, todo]))
+    //   .catch(error => console.log(error))
   }
 
   function toggleTodo(index) {
     const todo = todos[index]
 
-    fetch('/api/todos/' + todo._id, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ done: !todo.done }),
-    })
-      .then(response => {
-        if (!response.ok) throw new Error('PATCH todo not ok')
-        return response.json()
-      })
-      .then(todo => {
-        setTodos([
-          ...todos.slice(0, index),
-          { ...todo, done: todo.done },
-          ...todos.slice(index + 1),
-        ])
-      })
-      .catch(error => console.log(error))
+    // fetch('/api/todos/' + todo._id, {
+    //   method: 'PATCH',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({ done: !todo.done }),
+    // })
+    //   .then(response => {
+    //     if (!response.ok) throw new Error('PATCH todo not ok')
+    //     return response.json()
+    //   })
+    //   .then(todo => {
+    //     setTodos([
+    //       ...todos.slice(0, index),
+    //       { ...todo, done: todo.done },
+    //       ...todos.slice(index + 1),
+    //     ])
+    //   })
+    //   .catch(error => console.log(error))
   }
 }
 
